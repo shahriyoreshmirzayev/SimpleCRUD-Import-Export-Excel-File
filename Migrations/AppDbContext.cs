89466976fe771d773428fd68669deb337a2b6fc0
Shahriyor <139459691+shahriyoreshmirzayev@users.noreply.github.com>
2025-05-16 14:55:23 +0500

    Add project files.

---
+++ Migrations/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SImpleCRUD.DataAccess.Entities;
+
+namespace SImpleCRUD.Migrations
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
